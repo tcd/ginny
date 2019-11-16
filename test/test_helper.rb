15830df645957f2c710904aa52849f6d65843a83
Clay Dunston <claydunston@gmail.com>
2019-11-16 14:48:51 -0600

    Update test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
 
 SimpleCov.start do
   add_filter "/bin/"
+  add_filter "/exe/"
   add_filter "/test/"
 
-  track_files "lib/**/*.rb"
+  track_files "/lib/**/*.rb"
 end
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
@@ -31,4 +32,31 @@ Minitest::Reporters.use!([
 # @param path [String]
 def file_fixture(path)
   return File.expand_path(File.join(File.dirname(__dir__), "test", "fixtures", "files", path))
+end
+
+module MiniTest::Assertions
+  # `assert_equal` with a default message.
+  # @raise [ArgumentError] unless both arguments are strings
+  # @param want [String] Expected
+  # @param have [String] Actual
+  def assert_equal_and_print(want, have)
+    raise ArgumentError unless want.is_a?(String) && have.is_a?(String)
+
+    clear = "\e[0m"
+    bold = "\e[1m"
+    red = "\e[31m"
+    green = "\e[32m"
+
+    msg = <<~END
+      #{'=' * 80}
+      expected:
+      #{green + want + clear}
+      actual:
+      #{red + have + clear}
+      #{'=' * 80}
+    END
+
+    # assert_equal(want, have, "#{clear}\n#{'=' * 80}\nEXPECTED:\n\n#{want}\nACTUAL:\n\n#{have}\n#{'=' * 80}\n#{red}")
+    assert_equal(want, have, ("\n" + clear + msg + red))
+  end
 end
