75c9324708b5bf5108faef24b71795f766845f12
Clay Dunston <claydunston@gmail.com>
2019-11-16 14:50:15 -0600

    Update require method

--- lib/ginny.rb
+++ lib/ginny.rb
@@ -1,8 +1,12 @@
-require "ginny/version"
-require "ginny/error"
-require "ginny/symbolize"
-require "ginny/string"
-require "ginny/klass"
-require "ginny/load"
+# require "ginny/version"
+# require "ginny/error"
+# require "ginny/symbolize"
+# require "ginny/string"
+# require "ginny/Class"
+# require "ginny/load"
 
-module Ginny; end
+module Ginny
+  QUOTE = '"'.freeze
+end
+
+Dir.glob(File.join(__dir__, "ginny", "**/*.rb")).each { |file| require file }
