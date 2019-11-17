a4fce4a0d956ddfe554765f96e9c857a3dd2042d
Clay Dunston <claydunston@gmail.com>
2019-11-16 23:15:44 -0600

    Add load tests back

--- test/ginny/ginny_test.rb
+++ test/ginny/ginny_test.rb
@@ -6,40 +6,33 @@ class GinnyTest < Minitest::Test
     refute_nil(::Ginny::VERSION)
   end
 
-  # def test_class_constructor
-  #   want = File.read(file_fixture("out/person_1.rb"))
-  #   have = Ginny::Class.create(name: "Clay", description: "An awesome guy.").render()
-  #   assert_equal(want, have)
-  # end
+  def test_load_from_json
+    want = <<~RUBY.strip
+      class Person
+        # @return [String]
+        attr_accessor :name
+        # Number of years the person has been alive.
+        # @return [Integer]
+        attr_accessor :age
+      end
+    RUBY
+    have = Ginny::Class.create(Ginny.load_json(file_fixture("in/person.json"))).render()
+    assert_equal(want.strip, have.strip)
+  end
 
-  # def test_attribute_constructor
-  #   want = File.read(file_fixture("out/person_2.rb"))
-  #   have = Ginny::Class.create(
-  #     name: "Person",
-  #     attrs: [
-  #       { name: "name", type: "String" },
-  #       { name: "age", description: "Number of years the person has been alive.", type: "Integer" },
-  #   ]).render()
-  #   assert_equal(want.strip, have.strip)
-  # end
-
-  # def test_class_constructor_from_json
-  #   want = File.read(file_fixture("out/person_1.rb"))
-  #   have = Ginny::Class.create(Ginny.load_json(file_fixture("in/person_1.json"))).render()
-  #   assert_equal(want, have)
-  # end
-
-  # def test_attribute_constructor_from_json
-  #   want = File.read(file_fixture("out/person_2.rb"))
-  #   have = Ginny::Class.create(Ginny.load_json(file_fixture("in/person_2.json"))).render()
-  #   assert_equal(want.strip, have.strip)
-  # end
-
-  # def test_class_constructor_from_yaml
-  #   want = File.read(file_fixture("out/person_1.rb"))
-  #   have = Ginny::Class.create(Ginny.load_yaml(file_fixture("in/person_1.yml"))).render()
-  #   assert_equal(want, have)
-  # end
+  def test_load_from_yaml
+    want = <<~RUBY.strip
+      class Person
+        # @return [String]
+        attr_accessor :name
+        # Number of years the person has been alive.
+        # @return [Integer]
+        attr_accessor :age
+      end
+    RUBY
+    have = Ginny::Class.create(Ginny.load_yaml(file_fixture("in/person.yml"))).render()
+    assert_equal(want, have)
+  end
 
   # def test_yaml_with_multiline_string
   #   want = File.read(file_fixture("out/directive.rb"))
