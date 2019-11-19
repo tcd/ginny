bf67f555a38bc5fcf1f3ec682e816ff20b1615ee
Clay Dunston <claydunston@gmail.com>
2019-11-19 13:45:14 -0600

    Add code for wrapping modules

---
+++ lib/ginny/models/mod.rb
@@ -0,0 +1,32 @@
+module Ginny
+
+  # Used to generate a [module](https://ruby-doc.org/core-2.6.5/doc/syntax/modules_and_classes_rdoc.html).
+  #
+  # More accurately, wrap the `body` (first argument) with any following module definitions (additional arguments).
+  #
+  # @example
+  #   Ginny.mod("puts('Hello World')", "Level1", "Level2")
+  #   #=> module Level1
+  #         module Level2
+  #           puts('Hello World')
+  #         end
+  #       end
+  #
+  # @param body [String] Name of module namespaces.
+  # @param names [String,Array<String>] Name of module namespaces.
+  # @return [String]
+  def self.mod(body, *names)
+    names.flatten!
+    count = names.length
+    return "" unless count.positive?()
+    level = 0
+    head = []
+    tail = []
+    names.each do |name|
+      head.push("module #{name}".indent(level))
+      tail.unshift("end".indent(level))
+      level += 2
+    end
+    return (head + [body&.indent(level)] + tail).compact.join("\n")
+  end
+end
---
+++ test/ginny/mod_test.rb
@@ -0,0 +1,106 @@
+require "test_helper"
+
+class ModTest < Minitest::Test
+
+  def test_mod_1_level
+    want = <<~RUBY.strip
+      module Example
+      end
+    RUBY
+    have = Ginny.mod(nil, "Example")
+    assert_equal(want, have)
+  end
+
+  def test_mod_2_levels
+    want = <<~RUBY.strip
+      module Level1
+        module Level2
+        end
+      end
+    RUBY
+    have = Ginny.mod(nil, "Level1", "Level2")
+    assert_equal(want, have)
+  end
+
+  def test_mod_3_levels
+    want = <<~RUBY.strip
+      module Level1
+        module Level2
+          module Level3
+          end
+        end
+      end
+    RUBY
+    have = Ginny.mod(nil, "Level1", "Level2", "Level3")
+    assert_equal(want, have)
+  end
+
+  def test_mod_10_levels
+    want = <<~RUBY.strip
+      module Level1
+        module Level2
+          module Level3
+            module Level4
+              module Level5
+                module Level6
+                  module Level7
+                    module Level8
+                      module Level9
+                        module Level10
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    RUBY
+    names = (1..10).map { |i| "Level#{i}" }
+    have = Ginny.mod(nil, names)
+    assert_equal(want, have)
+  end
+
+  def test_body_1
+    want = <<~RUBY.strip
+      module Example
+        puts('Hello World')
+      end
+    RUBY
+    have = Ginny.mod("puts('Hello World')", "Example")
+    assert_equal(want, have)
+  end
+
+  def test_body_2
+    want = <<~RUBY.strip
+      module Level1
+        module Level2
+          module Level3
+            module Level4
+              module Level5
+                module Level6
+                  module Level7
+                    module Level8
+                      module Level9
+                        module Level10
+                          puts('Hello World')
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    RUBY
+    body = "puts('Hello World')"
+    names = (1..10).map { |i| "Level#{i}" }
+    have = Ginny.mod(body, names)
+    assert_equal(want, have)
+  end
+
+end
